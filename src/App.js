2bd6f0c2c69382c78afb1ea0aa7f9c9f18c93037
fatihbaydar <fatihbaydar2004@gmail.com>
2024-09-19 15:38:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
+      <span className="animate-bounce">🏀</span>
+      <h1 className="animate-pulse">A Store Project</h1>
+      <span className="animate-bounce">🏀</span>
+    </div>
+  );
+}
+
+export default App;
