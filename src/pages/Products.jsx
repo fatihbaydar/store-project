af33fd8f9737518e5237239563000451e784390d
fatihbaydar <fatihbaydar2004@gmail.com>
2024-09-23 07:54:48 +0300

    product

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,33 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import ProductCard from '../components/ProductCard';
+import ProductCard from "../components/ProductCard";
+import Loading from "../components/Loading";
+import SearchInput from "../components/SearchInput";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
-  const [search, setSearch] = useState("")
-  const getData = async() => {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const getData = async () => {
+    setLoading(true);
     try {
-     const res = await axios.get(`https://dummyjson.com/products/search?q=${search}`) 
+      const { data } = await axios.get(
+        `https://dummyjson.com/products/search?q=${search}`
+      );
+      setProducts(data.products);
     } catch (error) {
-      console.log(error)
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-  getData();
-  }, []);
-  
-  
+    getData();
+  }, [search]); //! search state'i değiştikçe getData fonksiyonu çalışsın
+
   return (
-    <div className='mx-auto max w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
-      <h2 className='text-2xl font-bold mt-8 tracking-tight text-gray-900'>
+    <div className="mx-auto max w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <SearchInput setSearch={setSearch} />
+      <h2 className="text-2xl font-bold mt-8 tracking-tight text-gray-900">
         All Products
       </h2>
-      <div className='mt-6 grid gri-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 xl:gap-x-8'>
-      {products.map(product => <ProductCard key={product.id} product={product} />)}
-      </div>
+      {loading ? (
+        <Loading />
+      ) : products.length ? (
+        <div className="mt-6 grid gri-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 xl:gap-x-8">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <h2 className="text-center text-3xl">No Products</h2>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import Navbar from "../components/Navbar";
 import PrivateRouter from "./PrivateRouter";
+import ProductDetail from "../pages/ProductDetail";
 
 const AppRouter = () => {
   return (
@@ -19,6 +20,7 @@ const AppRouter = () => {
         <Route path="" element={<PrivateRouter/>}>
           <Route path="/dashboard" element={<Home />} />
           <Route path="/dashboard/products" element={<Products />} />
+          <Route path="/dashboard/products/:id" element={<ProductDetail />} />
           <Route path="/dashboard/about" element={<About />} />
         </Route>
         <Route path="*" element={<NotFound />} />
