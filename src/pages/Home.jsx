27e4d35467dbb6d4e391bf29741c585a0572eca6
fatihbaydar <fatihbaydar2004@gmail.com>
2024-10-03 18:20:51 +0300

    testimonials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Stats from '../components/Stats'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
     <div>
       <Stats/>
+      <Testimonial/>
     </div>
   )
 }
